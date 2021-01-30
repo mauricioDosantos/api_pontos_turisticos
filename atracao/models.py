6eaee239534ace5e506c05c8140ce7b0ef8594c0
mauricioDosantos <mauriciodossantoscosta5@gmail.com>
2021-01-30 11:34:42 -0300

    criação de models de cada a plicação e criação dos arquivos da API

--- atracao/models.py
+++ atracao/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Atracao(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    horario_funcionamento = models.TextField()
+    data = models.DateTimeField(auto_now_add=True)
+    idade_min = models.IntegerField()
+    aprovado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
